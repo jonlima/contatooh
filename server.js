8d493f79e84f6c292d2af618dd774e8965db635f
jonlima <jonatha.lima14@gmail.com>
2015-09-12 19:49:01 -0300

    teste integracao

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = require('./config/express')(app);
 require('./config/passport')();
 require('./config/database.js')('mongodb://localhost/contatooh');
 
+
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta '+ app.get('port'));
 });
