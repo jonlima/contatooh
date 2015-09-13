27dfee85fd15a5cfbe2612cd2339d082d224770a
jonlima <jonatha.lima14@gmail.com>
2015-09-12 22:40:38 -0300

    correcao varial saucetestname

--- config/env/test.js
+++ config/env/test.js
@@ -1,7 +1,7 @@
 module.exports = {
 	env: 'test',
 	db: 'mongodb://localhost/contatooh_test',
-	sauceTesteName: 'Contatooh E2E Testing',
+	sauceTestName: 'Contatooh E2E Testing',
 	sauceUser: process.env.SAUCE_USERNAME,
 	sauceKey: process.env.SAUCE_ACCESS_KEY,
 	travisJobNumber: process.env.TRAVIS_JOB_NUMBER,
